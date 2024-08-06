ba4dd7c28659672a4d71a590c60f4d5657dd5f86
Dustin Franklin <df59@uakron.edu>
2024-08-06 09:33:39 -0400

    Remove old code

--- main.js
+++ main.js
@@ -10,24 +10,6 @@ import TutorialScene from "./scenes/TutorialScene";
 import GameOverScene from "./scenes/GameOverScene";
 import LeaderBoardScene from "./scenes/LeaderBoardScene";
 
-const sizes = {
-  width: 1920,
-  height: 960,
-};
-
-const initialPlayerSpeed = 250;
-const initialSpeedDown = 300;
-const spawnInterval = 1000;
-const maxTargets = 10;
-let spawnRate = 1;
-
-const missiles = [
-  { key: "missile1", speed: 200, damage: 10, health: 1 },
-  { key: "missile2", speed: 250, damage: 20, health: 2 },
-  { key: "missile3", speed: 300, damage: 30, health: 3 },
-  { key: "missile4", speed: 350, damage: 40, health: 4 },
-];
-
 const config = {
   type: Phaser.WEBGL,
   width: sizes.width,
@@ -43,17 +25,7 @@ const config = {
       debug: false,
     },
   },
-  scene: [
-    LoaderScene,
-    PlayScene,
-    UpgradeScene,
-    TurretScene,
-    PointModeScene,
-    MainMenuScene,
-    TutorialScene,
-    GameOverScene,
-    LeaderBoardScene,
-  ],
+  scene: [LoaderScene, PlayScene, UpgradeScene, TurretScene, PointModeScene, MainMenuScene, TutorialScene, GameOverScene, LeaderBoardScene],
 };
 
 const game = new Phaser.Game(config);
